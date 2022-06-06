a875eb7ff546174eaa68cc7ef33ff670d704fe18
EC2 Default User <ec2-user@ip-172-31-40-38.ec2.internal>
2022-06-06 04:43:58 +0000

    テスト再提出

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,23 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_up_path_for(resource)
+    case resource
+    when :Admin
+      admin_homes_path
+    when :Customer
+      customers_show_path
+    end
+  end
+
+
+
   def after_sign_in_path_for(resource)
     case resource
     when Admin
       admin_homes_path
     when Customer
-      customers_show_path
+      root_path
     end
   end
 
--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -4,6 +4,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+
   # GET /resource/sign_up
   # def new
   #   super
@@ -51,9 +52,9 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    customers_show_path(resource)
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
